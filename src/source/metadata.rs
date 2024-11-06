f7905002c4cdffb08f826a2ff4c265e013e8b259
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-11-06 13:23:38 +0000

    test(source): add tests for artist metadata

--- src/source/metadata.rs
+++ src/source/metadata.rs
@@ -12,47 +12,118 @@ pub struct Metadata {
 }
 
 impl Metadata {
-    #[allow(
-        clippy::cast_sign_loss,
-        clippy::as_conversions,
-        clippy::cast_possible_truncation
-    )]
     #[must_use]
     pub fn new(group: &Group, torrent: &Torrent) -> Self {
-        let artist = decode_html_entities(Self::get_artist(group)).to_string();
-        let album = decode_html_entities(&group.name).to_string();
-        let remaster_title = decode_html_entities(&torrent.remaster_title).to_string();
-        let year = if torrent.remaster_year.is_none() || torrent.remaster_year == Some(0) {
-            group.year
-        } else {
-            torrent.remaster_year.expect("Remaster year should be set")
-        };
-        let media = torrent.media.clone();
         Metadata {
-            artist,
-            album,
-            remaster_title,
-            year,
-            media,
+            artist: get_artist(group),
+            album: get_album(group),
+            remaster_title: get_remaster_title(torrent),
+            year: get_year(group, torrent),
+            media: torrent.media.clone(),
         }
     }
+}
 
-    fn get_artist(group: &Group) -> &str {
-        match &group.music_info {
-            None => "Unknown Artist",
-            Some(info) => {
-                if info.artists.len() > 1 {
-                    "Various Artists"
-                } else if info.artists.is_empty() {
-                    "Unknown Artist"
-                } else {
-                    info.artists
-                        .first()
-                        .expect("should be at least one artist")
-                        .name
-                        .as_str()
-                }
-            }
-        }
+fn get_artist(group: &Group) -> String {
+    let artist = get_artist_internal(group).unwrap_or("Unknown Artist".to_owned());
+    decode_html_entities(&artist).to_string()
+}
+
+fn get_artist_internal(group: &Group) -> Option<String> {
+    let info = group.music_info.clone()?;
+    if info.artists.len() > 1 {
+        Some("Various Artists".to_owned())
+    } else if info.artists.is_empty() {
+        None
+    } else {
+        info.artists.into_iter().map(|x| x.name).next()
+    }
+}
+
+fn get_album(group: &Group) -> String {
+    decode_html_entities(&group.name).to_string()
+}
+
+fn get_remaster_title(torrent: &Torrent) -> String {
+    decode_html_entities(&torrent.remaster_title).to_string()
+}
+
+fn get_year(group: &Group, torrent: &Torrent) -> u16 {
+    if torrent.remaster_year.is_none() || torrent.remaster_year == Some(0) {
+        group.year
+    } else {
+        torrent.remaster_year.expect("Remaster year should be set")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::source::metadata::get_artist;
+    use gazelle_api::{Artist, Group, MusicInfo};
+
+    #[test]
+    fn get_artist_none() {
+        // Arrange
+        let expected = "Unknown Artist".to_owned();
+        let group = Group {
+            music_info: Some(MusicInfo {
+                artists: Vec::new(),
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
+    }
+
+    #[test]
+    fn get_artist_one() {
+        // Arrange
+        let expected = "Hello, world!".to_owned();
+        let group = Group {
+            music_info: Some(MusicInfo {
+                artists: vec![Artist {
+                    id: 12345,
+                    name: expected.clone(),
+                }],
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
+    }
+
+    #[test]
+    fn get_artist_two() {
+        // Arrange
+        let expected = "Various Artists".to_owned();
+        let group = Group {
+            music_info: Some(MusicInfo {
+                artists: vec![
+                    Artist {
+                        id: 12345,
+                        name: "Artist One".to_owned(),
+                    },
+                    Artist {
+                        id: 12345,
+                        name: "Artist Two".to_owned(),
+                    },
+                ],
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
     }
 }
